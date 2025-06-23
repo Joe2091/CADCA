0499441458e72297775f29aa60edf7792dcf45ff
joe2091 <x21476132@student.ncirl.ie>
2025-06-23 16:50:39 +0100

    1st commit since college updating project

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
